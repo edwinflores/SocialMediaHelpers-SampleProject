40a4366cc9dc9a824dc87c4a35aa4a077d63a49e
edwinflores <edwin.flores@klab.com>
2015-02-10 16:01:14 +0800

    Added the Helper libraries

---
+++ vendor/Facebook/FacebookHelper.php
@@ -0,0 +1,112 @@
+<?php
+require_once __DIR__ . '/autoload.php';
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+use Facebook\Entities\AccessToken;
+use Facebook\HttpClients\FacebookCurlHttpClient;
+use Facebook\HttpClients\FacebookHttpable;
+
+class FacebookHelper
+{
+    /** @var $redirect_helper :: Used as a helper for Facebook redirection*/
+    private $redirect_helper;
+
+    /** @var  $scopes :: Scopes for data to be requested
+     *  Refer to: https://developers.facebook.com/docs/facebook-login/permissions/v2.2 */
+    private $scopes;
+
+    /** @var  $app_id_key & $app_secret_key :: The app's keys given by facebook */
+    private $app_id_key;
+    private $app_secret_key;
+
+    /** @var  $redirect_url :: The redirect url Facebook goes to in redirection */
+    private $redirect_url;
+
+    public function setAppKeys($facebook_id, $facebook_secret)
+    {
+        $this->app_id_key = $facebook_id;
+        $this->app_secret_key = $facebook_secret;
+    }
+
+    public function setScopes($scopes = array())
+    {
+        $this->scopes = $scopes;
+    }
+
+    public function setRedirectUrl($url)
+    {
+        $this->redirect_url = $url;
+    }
+
+    /**
+     * Initialize and/or get a Facebook Session
+     */
+    public function init()
+    {
+        // Initialize app with the app id and the secret id
+        FacebookSession::setDefaultApplication($this->app_id_key, $this->app_secret_key);
+        $this->redirect_helper = new FacebookRedirectLoginHelper($this->redirect_url);
+        try {
+            $_SESSION['fb_session_redirect'] = $this->redirect_helper->getSessionFromRedirect();
+        } catch( FacebookRequestException $ex ) {
+        } catch( Exception $ex ) {
+        }
+    }
+
+    /**
+     * Checks if an ongoing Facebook Session is present
+     */
+    public function IsFacebookSessionPresent()
+    {
+        return isset($_SESSION['fb_session_redirect']);
+    }
+
+    /**
+     * If the app isn't confirmed by the user, goes through this Url first
+     */
+    public function GetConfirmationUrl()
+    {
+        $scopes_string = implode(",", $this->scopes);
+        $loginUrl = $this->redirect_helper->getLoginUrl(array('scope' => $scopes_string));
+        header("Location: " . $loginUrl);
+    }
+
+    /**
+     * @param array $data :: The list of data needed
+     * @return array $user_data :: Returns an associative array with the user's data
+     */
+    public function GetUserData($data = array())
+    {
+        //Request the user data
+        $request = new FacebookRequest($_SESSION['fb_session_redirect'], 'GET', '/me');
+        $response = $request->execute();
+
+        //Fetch the response
+        $graphObject = $response->getGraphObject();
+        $user_data = array();
+        $data_count = count($data);
+        for ($i = 0; $i < $data_count; $i++)
+        {
+            $info = $graphObject->getProperty($data[$i]);
+            if ($info) {
+                $user_data[$data[$i]] = $info;
+            }
+        }
+
+        return $user_data;
+    }
+
+    /*
+     * For manual redirection
+     */
+    public function getRedirectUrl()
+    {
+        return $this->redirect_url;
+    }
+}
---
+++ vendor/Google/GoogleHelper.php
@@ -0,0 +1,79 @@
+<?php
+require_once(__DIR__ . '/autoload.php');
+
+class GoogleHelper
+{
+    private $client_id;
+    private $client_secret;
+    private $redirect_url;
+    private $simple_api_key;
+    private $app_name;
+    private $scopes;
+    private $objOAuthService;
+
+    public function setAppDetails($name, $client_id, $client_secret, $simple_api)
+    {
+        $this->app_name = $name;
+        $this->client_id = $client_id;
+        $this->client_secret = $client_secret;
+        $this->simple_api_key = $simple_api;
+    }
+
+    public function setRedirectURL($url)
+    {
+        $this->redirect_url = $url;
+    }
+
+    public function setScopes($google_scopes = array())
+    {
+        $this->scopes = $google_scopes;
+    }
+
+    public function getUserData()
+    {
+        //Client request for Google API access
+        $client = new Google_Client();
+        $client->setApplicationName($this->app_name);
+        $client->setClientId($this->client_id);
+        $client->setClientSecret($this->client_secret);
+        $client->setRedirectUri($this->redirect_url);
+        $client->setDeveloperKey($this->simple_api_key);
+        $client->addScope($this->scopes);
+
+        //Client Request Send
+        $this->objOAuthService = new Google_Service_Oauth2($client);
+
+        //Logout Request
+        if (isset($_REQUEST['logout'])) {
+            unset($_SESSION['gACCESSTOKEN']);
+            $client->revokeToken();
+            header('Location: ' . $this->redirect_url);
+        }
+
+        /**
+         * Google OAuth Flow Authenticate code
+         * Set Access Token to Session
+         */
+        if (isset($_GET['code'])) {
+            $client->authenticate(Param::get('code'));
+            $_SESSION['gACCESSTOKEN'] = $client->getAccessToken();
+            header('Location: ' . APP_URL_LOCAL . '/user/google_login');
+        }
+
+        // Set access token to make request
+        if (!empty($_SESSION['gACCESSTOKEN'])) {
+            $client->setAccessToken($_SESSION['gACCESSTOKEN']);
+        }
+
+        //Fetch user data from Google
+        if ($client->getAccessToken()) {
+            $userData = $this->objOAuthService->userinfo->get();
+            return $userData;
+        } else {
+            $authUrl = $client->createAuthUrl();
+            redirect($authUrl);
+        }
+
+        return null;
+    }
+}
